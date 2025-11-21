c6f1f8ed14ac66495d1d4864eb9c81ac7bdaa4ff
CypherWhisperer <cypherwhisperer@gmail.com>
2025-11-21 11:54:43 +0300

    UPDATE: Added tailwind and postcss to the project - to leverage both scoped styling and global utility-first classes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,19 +1,18 @@
-import styles from './Footer.module.scss';
-import React from 'react';
+import styles from "./Footer.module.scss";
+import React from "react";
 
 // DATA
-import { socialLinks,socialLinksIcons } from '../../assets/data';
+import { socialLinks, socialLinksIcons } from "../../assets/data";
 
 export default function Footer() {
-  return(
-    <>
-      <footer className={styles.footer_content}>
-        <SocialLinks />
-      </footer>
-    </>
-  )
+    return (
+        <>
+            <footer className={styles.footer_content}>
+                <SocialLinks />
+            </footer>
+        </>
+    );
 }
-
 
 function SocialLinks() {
     return (
@@ -21,16 +20,22 @@ function SocialLinks() {
             {/* <li className={styles.separator} /> */}
             <li className={styles.links}>
                 {socialLinks.map((link, iconIndex) => {
-                    return(
-                        <a key={link.id} href={link.target} aria-label={link.name}> 
-                          {/* This avoids pre-instantiating JSX elements with fixed keys, and makes mapping clearer.*/}
-                          {React.createElement(socialLinksIcons[iconIndex])}
+                    return (
+                        <a
+                            key={link.id}
+                            target="_blank"
+                            href={link.target}
+                            aria-label={link.name}
+                        >
+                            {/* This avoids pre-instantiating JSX elements with fixed keys, and makes mapping clearer.*/}
+                            {React.createElement(socialLinksIcons[iconIndex])}
                         </a>
-                    )
+                    );
                 })}
             </li>
-            <li> <small> &copy; 2025, all rights reserved </small> </li>
+            <li>
+                <small>&copy; 2025, all rights reserved</small>
+            </li>
         </ul>
-    )
+    );
 }
-
